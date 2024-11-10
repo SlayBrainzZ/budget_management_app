24e20f115264df484e3126a806a2d795f577d069
SlayBrainzZ <136199883+SlayBrainzZ@users.noreply.github.com>
2024-11-10 01:24:38 +0100

    Transactions class

---
+++ lib/backend/Transaction.dart
@@ -0,0 +1,51 @@
+// Transaction.dart
+class Transaction {
+  final String id;
+  final String userId;
+  final String bankAccountId;
+  final double amount;
+  final DateTime date;
+  final String time;
+  final String categoryId;
+  final String type;
+  final String? note;
+
+  Transaction({
+    required this.id,
+    required this.userId,
+    required this.bankAccountId,
+    required this.amount,
+    required this.date,
+    required this.time,
+    required this.categoryId,
+    required this.type,
+    this.note,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userId': userId,
+      'bankAccountId': bankAccountId,
+      'amount': amount,
+      'date': date.toIso8601String(),
+      'time': time,
+      'categoryId': categoryId,
+      'type': type,
+      'note': note ?? '',
+    };
+  }
+
+  static Transaction fromMap(Map<String, dynamic> data, String documentId) {
+    return Transaction(
+      id: documentId,
+      userId: data['userId'],
+      bankAccountId: data['bankAccountId'],
+      amount: data['amount'],
+      date: DateTime.parse(data['date']),
+      time: data['time'],
+      categoryId: data['categoryId'],
+      type: data['type'],
+      note: data['note'],
+    );
+  }
+}
