df9bcd37169efde71bc2c7ba47bd9f7d8c2edeb3
SlayBrainzZ <136199883+SlayBrainzZ@users.noreply.github.com>
2024-11-10 01:24:16 +0100

    Users class

---
+++ lib/backend/User.dart
@@ -0,0 +1,36 @@
+class User {
+
+  final String id;
+  final String userId;
+  final String email;
+  final String encryptedPassword;
+  final DateTime createdDate;
+
+  User({
+    required this.id,
+    required this.userId,
+    required this.email,
+    required this.encryptedPassword,
+    required this.createdDate
+  });
+
+  Map<String, dynamic> toMap(){
+    return{
+      'email': email,
+      'encryptedPassword': encryptedPassword,
+      'createdDate': createdDate.toIso8601String(),
+    };
+  }
+
+  static User fromMap(Map<String, dynamic> data, String documentId){
+    return User(
+      id: documentId,
+      userId: data['userId'],
+      email: data['email'],
+      encryptedPassword: data['encryptedPassword'],
+      createdDate: DateTime.parse(data['createdDate']),
+    );
+  }
+
+
+}
