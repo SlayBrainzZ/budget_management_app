0fdc4529889e1aa851472e1b74ac4fb5409226a8
SlayBrainzZ <136199883+SlayBrainzZ@users.noreply.github.com>
2024-11-23 15:43:58 +0100

    Update Transaction

--- lib/backend/Transaction.dart
+++ lib/backend/Transaction.dart
@@ -10,21 +10,17 @@
 class Transaction {
   String? id; // Make id nullable to handle creation scenario
   final String userId;
-  String bankAccountId;
   double amount;
   final DateTime date;
-  final String time;
-  String? categoryId; // Make categoryId nullable
+  String? categoryId;
   final String type;
   String? note;
   final bool importance;
 
   Transaction({
     required this.userId,
-    required this.bankAccountId,
     required this.amount,
     required this.date,
-    required this.time,
     this.categoryId, // Make categoryId optional
     required this.type,
     required this.importance,
@@ -34,10 +30,8 @@ class Transaction {
   Map<String, dynamic> toMap() {
     return {
       'userId': userId,
-      'bankAccountId': bankAccountId,
       'amount': amount.toString(),
       'date': date.toIso8601String(),
-      'time': time,
       'categoryId': categoryId,
       'type': type,
       'importance' : importance,
@@ -48,10 +42,8 @@ class Transaction {
   static Transaction fromMap(Map<String, dynamic> data, String documentId) {
     return Transaction(
       userId: data['userId'],
-      bankAccountId: data['bankAccountId'],
       amount: double.parse(data['amount']),
       date: DateTime.parse(data['date']),
-      time: data['time'],
       categoryId: data['categoryId'],
       type: data['type'],
       importance: data['importance'],
