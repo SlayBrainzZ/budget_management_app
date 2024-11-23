2e010fe502b57dcfd22f815a9aa7d7145936fcbe
SlayBrainzZ <136199883+SlayBrainzZ@users.noreply.github.com>
2024-11-23 15:33:44 +0100

    Update Category

--- lib/backend/Category.dart
+++ lib/backend/Category.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 /**
  * This class represents a spending category. It stores information
  * such as name, and budget limit. The ID is now retrieved from Firestore
@@ -6,16 +8,21 @@
  * @author Ahmad
  */
 
+
 class Category {
   String? id; // Make id nullable to handle creation scenario
   String userId; // Add userId to track who created the category
   String name;
   double budgetLimit;
+  IconData? icon;
+  Color? color;
 
   Category({
     required this.userId, // Make userId required in the constructor
     required this.name,
     required this.budgetLimit,
+     this.icon,
+     this.color,
   });
 
   Map<String, dynamic> toMap() {
@@ -23,6 +30,8 @@ class Category {
       'userId': userId, // Include userId in the map
       'name': name,
       'budgetLimit': budgetLimit.toString(),
+      'icon': icon?.codePoint,
+      'color': color?.value,
     };
   }
 
@@ -31,6 +40,8 @@ class Category {
       userId: data['userId'],
       name: data['name'],
       budgetLimit: double.parse(data['budgetLimit']),
+      icon: data['icon'] != null ? IconData(data['icon'], fontFamily: 'MaterialIcons') : null, // Handle null icon
+      color: data['color'] != null ? Color(data['color']) : null, // Handle null color
     )..id = documentId; // Assign the document ID after creation
   }
 }
