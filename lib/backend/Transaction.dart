9f55460c7b401966823bcfff206a5025c5b5ff2f
Sofia <papadopoulou.s455@gmail.com>
2024-11-27 14:29:38 +0100

    transaktionen erfolgreich aufgelistet, Transaktion.dart musste um ein parameter erweitert werden

--- lib/backend/Transaction.dart
+++ lib/backend/Transaction.dart
@@ -1,3 +1,4 @@
+import 'Category.dart';
 /**
  * This class represents a financial transaction. It stores information
  * such as ID, user ID, bank account ID, amount, date, time, category ID, transaction type,
@@ -5,8 +6,55 @@
  *
  * @author Ahmad
  */
+class Transaction {
+  String? id;
+  final String userId;
+  double amount;
+  final DateTime date;
+  String? categoryId;
+  final String type;
+  String? note;
+  final bool importance;
 
+  Category? categoryData; // Neu: Verknüpfte Kategorie-Daten
 
+  Transaction({
+    required this.userId,
+    required this.amount,
+    required this.date,
+    this.categoryId,
+    required this.type,
+    required this.importance,
+    this.note,
+    this.categoryData, // Neu
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'userId': userId,
+      'amount': amount.toString(),
+      'date': date.toIso8601String(),
+      'categoryId': categoryId,
+      'type': type,
+      'importance': importance,
+      'note': note ?? '',
+    };
+  }
+
+  static Transaction fromMap(Map<String, dynamic> data, String documentId) {
+    return Transaction(
+      userId: data['userId'],
+      amount: double.parse(data['amount']),
+      date: DateTime.parse(data['date']),
+      categoryId: data['categoryId'],
+      type: data['type'],
+      importance: data['importance'],
+      note: data['note'],
+    )..id = documentId;
+  }
+}
+
+/*
 class Transaction {
   String? id; // Make id nullable to handle creation scenario
   final String userId;
@@ -50,4 +98,4 @@ class Transaction {
       note: data['note'],
     )..id = documentId; // Assign the document ID after creation
   }
-}
+}*/
