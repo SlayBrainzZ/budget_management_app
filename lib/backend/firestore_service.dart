9c803533117ef71a18643aaf88770bf9617473bc
SlayBrainzZ <136199883+SlayBrainzZ@users.noreply.github.com>
2024-12-06 00:31:25 +0100

    Add csv functions (web)

--- lib/backend/firestore_service.dart
+++ lib/backend/firestore_service.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart' as firestore;
 import 'package:budget_management_app/backend/User.dart';
 import 'package:budget_management_app/backend/Transaction.dart';
